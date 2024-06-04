f2849f93eeab66cd69a264c5a2f37fa38bbc9c82
Mostakimw <scientist.expert@gmail.com>
2024-06-04 18:53:57 +0600

    reusable form and login page designed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,56 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const LoginPage = () => {
   return (
-    <div>
-      <h1>This is login page</h1>
-    </div>
+    <Box
+      sx={{ backgroundColor: "#E7E2FF" }}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Box
+        sx={{
+          maxWidth: "800px",
+          width: "100%",
+          height: "500px",
+          backgroundColor: "white",
+          boxShadow: "4px white",
+          borderRadius: 3,
+        }}
+      >
+        <Stack direction="row" sx={{ height: "100%" }}>
+          <Box
+            sx={{
+              padding: "20px 15px",
+              maxWidth: "400px",
+              width: "100%",
+              height: "full",
+            }}
+          >
+            <Typography variant="h5" fontWeight={600}>
+              Login Page
+            </Typography>
+            <Typography component="p">
+              Login to explore more from our site
+            </Typography>
+          </Box>
+          {/* right side */}
+          <Box
+            sx={{
+              backgroundColor: "purple",
+              maxWidth: "400px",
+              width: "100%",
+              borderRadius: "0 24px 24px 0",
+              height: "full",
+            }}
+          >
+            <Typography>Hello</Typography>
+          </Box>
+        </Stack>
+      </Box>
+    </Box>
   );
 };
 
